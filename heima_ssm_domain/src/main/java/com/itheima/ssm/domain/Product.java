b720dcae5da4b40cdc9df222e2f71723718926a8
90925 <909256107@qq.com>
2019-09-29 14:17:04 +0800

    添加至本地仓库

---
+++ heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
@@ -0,0 +1,31 @@
+package com.itheima.ssm.domain;
+
+
+import java.util.Date;
+
+public class Product {
+    private String id; // 主键
+    private String productNum; // 编号 唯一
+    private String productName; // 名称
+    private String cityName; // 出发城市
+
+    private Date departureTime; // 出发时间
+    private String departureTimeStr;
+    private double productPrice; // 产品价格
+    private String productDesc; // 产品描述
+    private Integer productStatus; // 状态 0 关闭 1 开启
+    private String productStatusStr;
+
+    public Product(String id, String productNum, String productName, String cityName, Date departureTime, String departureTimeStr, double productPrice, String productDesc, Integer productStatus, String productStatusStr) {
+        this.id = id;
+        this.productNum = productNum;
+        this.productName = productName;
+        this.cityName = cityName;
+        this.departureTime = departureTime;
+        this.departureTimeStr = departureTimeStr;
+        this.productPrice = productPrice;
+        this.productDesc = productDesc;
+        this.productStatus = productStatus;
+        this.productStatusStr = productStatusStr;
+    }
+}
